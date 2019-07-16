ed4e0f10f9305609cff3b1bca7816fff99e5654f
babazorus <Jouini.baydir@gmail.com>
2019-07-16 18:30:09 +0200

    modify

--- LEVEL3/atoi_base.c
+++ LEVEL3/atoi_base.c
@@ -1,43 +1,40 @@
-int isblank(char c)
+int is_blank(char c)
 {
-	if (c <= 32)
-		return (1);
-	return (0);
+    if (c <= 32)
+        return (1);
+    return (0);
 }
 
-int		isvalid(char c, int base)
+int is_valid(char c, int base)
 {
-	char digits[17] = "0123456789abcdef";
-	char digits2[17] = "0123456789ABCDEF";
-
-	while (base--)
-		if (digits[base] == c || digits2[base] == c)
-			return (1);
-	return (0);
+    char digits[17] = "0123456789abcdef";
+    char digits2[17] = "0123456789ABCDEF";
+    while (base--)
+        if (digits[base] == c || digits2[base] == c)
+            return (1);
+    return (0);
 }
 
-int		value_of(char c)
+int value_of(char c)
 {
-	if (c >= '0' && c <= '9')
-		return (c - '0');
-	else if (c >= 'a' && c <= 'f')
-		return (c - 'a' + 10);
-	else if (c >= 'A' && c <= 'F')
-		return (c - 'A' + 10);
-	return (0);
+    if (c >= '0' && c <= '9')
+        return (c - '0');
+    else if (c >= 'a' && c <= 'f')
+        return (c - 'a' + 10);
+    else if (c >= 'A' && c <= 'F')
+        return (c - 'A' + 10);
+    return (0);
 }
 
-int		ft_atoi_base(const char *str, int str_base)
+int ft_atoi_base(const char *str, int str_base)
 {
-	int result;
-	int sign;
-
-	result = 0;
-	while (isblank(*str))
-		str++;
-	sign = (*str == '-') ? -1 : 1;
-	(*str == '-' || *str == '+') ? ++str : 0;
-	while (isvalid(*str, str_base))
-		result = result * str_base + value_of(*str++);
-	return (result * sign);
+    int res = 0;
+    int sign;
+    while (is_blank(*str))
+        str++;
+    sign = (*str == '-') ? -1 : 1;
+    (*str == '-' || *str == '+') ? ++str : 0;
+    while (is_valid(*str, str_base))
+        res = res * str_base + value_of(*str++);
+    return (res * sign);
 }
