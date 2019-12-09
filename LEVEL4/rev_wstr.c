54c4076142d9d28b82a059b12d6969a34ec55ece
babazorus <Jouini.baydir@gmail.com>
2019-12-09 20:11:46 +0100

    nouvelle version de rev_wstr

--- LEVEL4/rev_wstr.c
+++ LEVEL4/rev_wstr.c
@@ -1,31 +1,34 @@
 #include <unistd.h>
- 
-void		rev_wstr(char *s, int first)
-{
-	int		start;
-	int		i;
 
-	i = 0;
+int is_blank(char c)
+{
+	return (c == ' ' || c == '\t');
+}
+
+void	rev_wstr(char *s, int first)
+{
+	int start;
+	int i = 0;
 	if (s[i])
 	{
-		while (s[i] && (s[i] == ' ' || s[i] == '\t'))
+		while (s[i] && (is_blank(s[i])))
 			i++;
 		if (!s[i])
 			return ;
 		start = i;
-		while (s[i] && s[i] != ' ' && s[i] != '\t')
+		while (s[i] && (!is_blank(s[i])))
 			i++;
 		rev_wstr(&s[i], 0);
-		write(1, &s[start], i - start);
+		write (1, &s[start], i - start);
 		if (!first)
-			write(1, " ", 1);
+			write (1, " ", 1);
 	}
 }
 
-int			main(int argc, char **argv)
+int		main(int ac, char **av)
 {
-	if (argc == 2)
-		rev_wstr(argv[1], 1);
+	if (ac == 2)
+		rev_wstr(av[1], 1);
 	write(1, "\n", 1);
 	return (0);
 }
